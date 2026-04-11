3a678c8efa90bcdc3783367c9d570a087f9c2d2b
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-11 18:07:29 +0000

    Data Types

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3) // can be used in Browser(pop up msg) but Currently we are using Node js
+
+let name = "Ansh"
+let age = 21
+let isLoggedIn = false
+
+// number => range 2^53
+// bigint => larger range
+// string => "" or ''
+// boolean => true / false 
+// null  = > standalone value
+// undefined => value not given
+// symbol => in tools and react and all
+
+
+
+// object 
+
+
+// How to see Variable Type 
+console.log(typeof "Ansh");
+console.log(typeof null)
+console.log(typeof undefined);
+
