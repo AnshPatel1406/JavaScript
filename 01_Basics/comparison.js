39acec91d9bcd73abd4d5b4c0defcdcf0685a7f6
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-12 06:06:11 +0000

    comparisons

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,27 @@
+console.log(5 > 10);
+console.log(10 > 5);
+
+
+
+
+console.log("1">1); // False
+console.log("1">0); // True 
+
+console.log(null > 0); // false
+console.log(null ==  0); // false
+console.log(null >= 0);  // true 
+
+// in such conversion comparisons we do not get predicted outputs | Better Avoid Such Conversions 
+
+// condition with undefined always give false
+
+///////////////////////////////////////////////////////////// 
+
+/// === gives strict check 
+
+console.log("1" === 1); // False due to strict check --> Must be of Same DataType
+
+
+
+
+
