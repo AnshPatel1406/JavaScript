abda4afaa38fb0cff778626bf7f555221c6ff215
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-16 14:13:29 +0000

    gdfgdfg

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,14 @@
+const marvel_heros = ["Thor","Iron Man","Spiderman"]
+const dc = ["Superman","Flash","Batman"]
+
+ marvel_heros.push(dc) // Pushes Array into an Array
+
+ console.log(marvel_heros);
+ console.log(marvel_heros[3][1]);
+
+
+ marvel_heros.concat(dc)
+
+ console.log(marvel_heros);
+ 
+ 
