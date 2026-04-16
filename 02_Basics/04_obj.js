4fbd654a4c66041cb7830164d499ac676085e505
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-16 15:08:41 +0000

    Objectt Above Basics

---
+++ 02_Basics/04_obj.js
@@ -0,0 +1,64 @@
+const tinderUser = new Object() // Singelton Object
+// const tinderUser = {}  // Non Singleton Object
+
+console.log(tinderUser);
+
+tinderUser.id = 19
+tinderUser.name = "Ansh Patel"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"xyz@gmail.com",
+    fullname:{
+        firstname:"Ansh",
+        lastname:"Patel"
+    }
+}
+
+console.log(regularUser.fullname.firstname);
+
+// Combining 2 Objects
+
+const obj1 = {
+    1:"A",2:"B"
+}
+const obj2 = {
+    3:'a',4:'b'}
+
+// const obj3 = {obj1,obj2} // Same Problem as in Arrays , Object Withing Object 
+
+// const obj3 = Object.assign(obj1,obj2)
+const obj3 = Object.assign({},obj1,obj2) // {} Suggest Due to Some Reasons , Good to DO
+// {} = Target , obj1 and obj2 are source , Source Will Be Put into The Target
+// if {} Not given Then Values from Obj2 Will Go to Obj1 Which is Slightly ODD
+
+// Syntax : Object.assign(target,source)
+
+const DiffrentMethod = {...obj1,...obj2}
+console.log(DiffrentMethod);
+
+console.log(obj3);
+
+
+// If an Object has Multiple Objects
+// Obj[1].key ==> to Access
+
+
+// Display all the Keys of Objects
+
+console.log(Object.keys(tinderUser)); // Returns an Array
+console.log(Object.values(tinderUser)); // Returns an Array
+console.log(Object.entries(tinderUser)); // Makes Array Withing Array of Keys and Values
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // To check if an Object has Certain Property
+
+
+
+
+
+
+
+
+
