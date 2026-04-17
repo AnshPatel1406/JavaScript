7a70b45497a110db5a7cd1ce38ed624b4daaae37
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-17 12:18:08 +0000

    if else + switch

---
+++ 04_Control_flow.js/one.js
@@ -0,0 +1,51 @@
+// if
+
+if(false)
+{
+    console.log("True")
+}
+
+else {
+    console.log("False")
+}
+
+// all comaprison operators 
+
+// === is for strict comparisions , work only when datatypes are similar 
+
+
+const balance = 100
+
+if(balance>0) console.log("True"),  // can use , also here 
+console.log("If Block Over")
+
+
+if (false){
+    
+}
+else if(true)
+{
+    console.log(("Here"));
+    
+}
+else {
+    console.log("Never Visited")
+}
+
+// AND & OR
+
+if(true && false && true)
+{
+    console.log("All True")
+}
+else {
+    console.log("All not True")
+}
+
+if (true || false)
+{
+    console.log("At least One True")
+}
+else {
+    console.log("All False")
+}
