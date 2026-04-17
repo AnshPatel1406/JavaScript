099bc8379fdb50433f41804afa1e2726de6924b0
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-17 12:59:31 +0000

    while do while

---
+++ 05_iterations./02_while_do_while.js
@@ -0,0 +1,19 @@
+// while (condition) {
+    
+// }
+
+let index = 0
+while(index <=10)
+{
+    console.log(index); // upto 10
+    
+    index++
+}
+console.log(index); // 11
+
+// do {
+    
+// } while (condition);
+
+// it has ; after while 
+
