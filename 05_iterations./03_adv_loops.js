a77d1b9903412b817a1a73e98da0a2f566fa4c29
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-17 15:47:29 +0000

    for of and for in LOOPS

--- 05_iterations./03_adv_loops.js
+++ 05_iterations./03_adv_loops.js
@@ -22,15 +22,68 @@ for(let greet of greetings)
     console.log(greet);   
 }
 
-// MAPS - DataTypes
+// MAPS - DataTypes - Unique values
 
-// for in LOOP
+const map = new Map()
 
-// for (const key in object) {
-//     if (!Object.hasOwn(object, key)) continue;
+// Holds Key Value Pairs // Holds Information of Order the values are inserted
+
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('IN',"India") // Will not Duplicate it as it Has Unique Values
+
+console.log(map);
+
+
+for (const [key,balue] of map) {
+    console.log(key, ':-',balue);
+}
+
+const myObj = {
+    "game1" : "NFS",
+    'game2' : 'Spiderman'
+}
+
+// OBJECTS ARE NOT ITERABLES LIKE MAPS 
+/*
+for (const element of myObj) {
+    console.log(element);
     
-//     const element = object[key];
+}
+*/
+
+
+// FOR IN Loop
+
+for (const key in myObj) {
+    // console.log(key);
     
+    console.log(`${key}'s value is ${myObj[key]}`);   
+}
+
+const programing = ["JS","ruby","CPP","Python"]
+
+for(const xyz in programing)
+{
+    console.log(xyz); // for in will get keys
     
-// }
+}
+
+for(const xyz of programing)
+{
+    console.log(xyz); // for of will get values
+    
+}
+
+// to get values in for in loop we use this...
+
+for(const [xyz,yz] in programing)
+{
+    console.log(xyz,programing[xyz])
+}
+
+
+
+// FOR IN :- for Objects 
+// FOR OF :- for Arrays
 
