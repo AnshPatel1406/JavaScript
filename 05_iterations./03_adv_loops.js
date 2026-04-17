36c5c2543a023f41af9c9cdac9d2986b6c065c69
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-17 13:05:52 +0000

    for of loop and Maps datatypes upcomming

---
+++ 05_iterations./03_adv_loops.js
@@ -0,0 +1,36 @@
+// for of LOOP
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+// for (const element of object) {
+    
+// }
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+// Print String Characters One By One
+
+const greetings = "Hellowo"
+
+for(let greet of greetings)
+{
+    console.log(greet);   
+}
+
+// MAPS - DataTypes
+
+// for in LOOP
+
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
+
