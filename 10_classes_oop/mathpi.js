aeeaf5a04cc94c815bdea77396e33fdd5d715c42
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-04-29 15:57:15 +0000

    objects properties

---
+++ 10_classes_oop/mathpi.js
@@ -0,0 +1,48 @@
+const desc = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(desc);
+
+// console.log(Math.PI);
+// Math.PI = 4
+// console.log(Math.PI);
+
+
+// factory function to define object 
+
+// const myNewObj = Object.create(null)
+
+//regular
+
+const chai = {
+    name :"Ginger Tea",
+    price : 249,
+    isAvailable : true,
+    orderchai : function(){
+        console.log("Chai nahi bani")
+    }
+}
+
+
+console.log(Object.getOwnPropertyDescriptor(chai)); // undefined // it gives description of propery so we need to give it
+console.log(Object.getOwnPropertyDescriptor(chai,'name')); // works 
+
+Object.defineProperty(chai,'name',{
+    writable:false,
+    // enumerable : false
+})
+
+
+console.log(Object.getOwnPropertyDescriptor(chai,'name')); // works 
+
+// Objects are not Iteratable
+// for (const [key,value] of chai) {
+//     console.log(`${key}:${value}`);
+//     }
+
+for (const [key,value] of Object.entries(chai)) {
+    console.log(`${key}:${value}`) // also return whole function 
+}
+
+// we can write 
+// if(typeof value !== ' function'){
+// console.log(asfsdfg);
+// }
